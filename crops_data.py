7a2facf8f799b7c98e13262e0cf8ba73b70d960a
Danilo III O. Gonzales <dogonzalesiii@gmail.com>
2026-01-16 22:28:50 +0000

    updated crops_data.py

--- crops_data.py
+++ crops_data.py
@@ -26,194 +26,6 @@
 # FAO56rev guidelines.
 
 crops = {
-
-    # ROOTS, TUBERS, BULBS
-
-    "carrot_common": {
-        "t_base": 6,
-        "t_upper": 30,
-        "stages": {
-            "initial": 320,
-            "development": 785,      # 320 + 465
-            "mid_season": 1285,      # 320 + 465 + 500
-            "harvest": 1575,         # total
-        },
-    },
-
-    "garlic_short": {
-        "t_base": 4,
-        "t_upper": 30,
-        "stages": {
-            "initial": 150,
-            "development": 490,
-            "mid_season": 825,
-            "harvest": 1140,
-        },
-    },
-
-    "garlic_long": {
-        "t_base": 4,
-        "t_upper": 30,
-        "stages": {
-            "initial": 580,
-            "development": 1195,
-            "mid_season": 1510,
-            "harvest": 1750,
-        },
-    },
-
-    "onion_common": {
-        "t_base": 4.5,
-        "t_upper": 35,
-        "stages": {
-            "initial": 460,
-            "development": 930,
-            "mid_season": 1810,
-            "harvest": 2290,
-        },
-    },
-
-    "potato_short": {
-        "t_base": 2,
-        "t_upper": 30,
-        "stages": {
-            "initial": 260,
-            "development": 750,
-            "mid_season": 1275,
-            "harvest": 1600,
-        },
-    },
-
-    "potato_long": {
-        "t_base": 2,
-        "t_upper": 30,
-        "stages": {
-            "initial": 405,
-            "development": 935,
-            "mid_season": 1425,
-            "harvest": 2260,
-        },
-    },
-
-
-    # LEAVES AND FLOWERS
-
-    "broccoli_short": {
-        "t_base": 4.5,
-        "t_upper": 30,
-        "stages": {
-            "initial": 195,
-            "development": 445,
-            "mid_season": 655,
-            "harvest": 755,
-        },
-    },
-
-    "broccoli_long": {
-        "t_base": 4.5,
-        "t_upper": 30,
-        "stages": {
-            "initial": 295,
-            "development": 645,
-            "mid_season": 1170,
-            "harvest": 1280,
-        },
-    },
-
-    "lettuce_short": {
-        "t_base": 4,
-        "t_upper": 28,
-        "stages": {
-            "initial": 365,
-            "development": 750,
-            "mid_season": 965,
-            "harvest": 985,
-        },
-    },
-
-    "lettuce_long": {
-        "t_base": 4,
-        "t_upper": 28,
-        "stages": {
-            "initial": 360,
-            "development": 815,
-            "mid_season": 1270,
-            "harvest": 1290,
-        },
-    },
-
-    # FRUIT VEGETABLES
-
-    "bell_pepper_common": {
-        "t_base": 10,
-        "t_upper": 35,
-        "stages": {
-            "initial": 445,
-            "development": 1625,
-            "mid_season": 2370,
-            "harvest": 2415,
-        },
-    },
-
-    "melon_short": {
-        "t_base": 10,
-        "t_upper": 38,
-        "stages": {
-            "initial": 185,
-            "development": 705,
-            "mid_season": 1020,
-            "harvest": 1175,
-        },
-    },
-
-    "melon_long": {
-        "t_base": 10,
-        "t_upper": 38,
-        "stages": {
-            "initial": 140,
-            "development": 685,
-            "mid_season": 1145,
-            "harvest": 1600,
-        },
-    },
-
-    "tomato_industry": {
-        "t_base": 7,
-        "t_upper": 28,
-        "stages": {
-            "initial": 280,
-            "development": 800,
-            "mid_season": 1680,
-            "harvest": 1900,
-        },
-    },
-
-    "tomato_market": {
-        "t_base": 7,
-        "t_upper": 28,
-        "stages": {
-            "initial": 325,
-            "development": 985,
-            "mid_season": 1865,
-            "harvest": 2065,
-        },
-    },
-
-
-    # FIELD CROPS – CEREALS
-
-
-    "barley_short": {
-        "t_base": 0,
-        "t_upper": 30,
-        "stages": {
-            "initial": 290,
-            "development": 745,
-            "mid_season": 1090,
-            "harvest": 1450,
-        },
-    },
-
     "barley_long": {
         "t_base": 0,
         "t_upper": 30,
@@ -224,7 +36,166 @@ crops = {
             "harvest": 2330,
         },
     },
-
+    "barley_short": {
+        "t_base": 0,
+        "t_upper": 30,
+        "stages": {
+            "initial": 290,
+            "development": 745,
+            "mid_season": 1090,
+            "harvest": 1450,
+        },
+    },
+    "bean_seed_long": {
+        "t_base": 10,
+        "t_upper": 32,
+        "stages": {
+            "initial": 320,
+            "development": 730,
+            "mid_season": 1130,
+            "harvest": 1350,
+        },
+    },
+    "bean_seed_short": {
+        "t_base": 10,
+        "t_upper": 32,
+        "stages": {
+            "initial": 160,
+            "development": 420,
+            "mid_season": 780,
+            "harvest": 960,
+        },
+    },
+    "bell_pepper_common": {
+        "t_base": 10,
+        "t_upper": 35,
+        "stages": {
+            "initial": 445,
+            "development": 1625,
+            "mid_season": 2370,
+            "harvest": 2415,
+        },
+    },
+    "broccoli_long": {
+        "t_base": 4.5,
+        "t_upper": 30,
+        "stages": {
+            "initial": 295,
+            "development": 645,
+            "mid_season": 1170,
+            "harvest": 1280,
+        },
+    },
+    "broccoli_short": {
+        "t_base": 4.5,
+        "t_upper": 30,
+        "stages": {
+            "initial": 195,
+            "development": 445,
+            "mid_season": 655,
+            "harvest": 755,
+        },
+    },
+    "canola_long": {
+        "t_base": 2,
+        "t_upper": 30,
+        "stages": {
+            "initial": 460,
+            "development": 1015,
+            "mid_season": 1920,
+            "harvest": 2515,
+        },
+    },
+    "canola_short": {
+        "t_base": 2,
+        "t_upper": 30,
+        "stages": {
+            "initial": 330,
+            "development": 640,
+            "mid_season": 1090,
+            "harvest": 1685,
+        },
+    },
+    "carrot_common": {
+        "t_base": 6,
+        "t_upper": 30,
+        "stages": {
+            "initial": 320,
+            "development": 785,
+            "mid_season": 1285,
+            "harvest": 1575,
+        },
+    },
+    "cotton_long": {
+        "t_base": 12.5,
+        "t_upper": 35,
+        "stages": {
+            "initial": 345,
+            "development": 1250,
+            "mid_season": 1865,
+            "harvest": 2150,
+        },
+    },
+    "cotton_short": {
+        "t_base": 12.5,
+        "t_upper": 35,
+        "stages": {
+            "initial": 280,
+            "development": 800,
+            "mid_season": 1600,
+            "harvest": 1950,
+        },
+    },
+    "garlic_long": {
+        "t_base": 4,
+        "t_upper": 30,
+        "stages": {
+            "initial": 580,
+            "development": 1195,
+            "mid_season": 1510,
+            "harvest": 1750,
+        },
+    },
+    "garlic_short": {
+        "t_base": 4,
+        "t_upper": 30,
+        "stages": {
+            "initial": 150,
+            "development": 490,
+            "mid_season": 825,
+            "harvest": 1140,
+        },
+    },
+    "lettuce_long": {
+        "t_base": 4,
+        "t_upper": 28,
+        "stages": {
+            "initial": 360,
+            "development": 815,
+            "mid_season": 1270,
+            "harvest": 1290,
+        },
+    },
+    "lettuce_short": {
+        "t_base": 4,
+        "t_upper": 28,
+        "stages": {
+            "initial": 365,
+            "development": 750,
+            "mid_season": 965,
+            "harvest": 985,
+        },
+    },
+    "maize_grain_long": {
+        "t_base": 10,
+        "t_upper": 32,
+        "stages": {
+            "initial": 295,
+            "development": 715,
+            "mid_season": 1490,
+            "harvest": 1955,
+        },
+    },
     "maize_grain_short": {
         "t_base": 10,
         "t_upper": 32,
@@ -235,15 +206,264 @@ crops = {
             "harvest": 1540,
         },
     },
-
-    "maize_grain_long": {
+    "maize_silage_long": {
         "t_base": 10,
         "t_upper": 32,
         "stages": {
-            "initial": 295,
-            "development": 715,
-            "mid_season": 1490,
-            "harvest": 1955,
+            "initial": 250,
+            "development": 750,
+            "mid_season": 1350,
+            "harvest": 1550,
+        },
+    },
+    "maize_silage_short": {
+        "t_base": 10,
+        "t_upper": 32,
+        "stages": {
+            "initial": 200,
+            "development": 580,
+            "mid_season": 1010,
+            "harvest": 1130,
+        },
+    },
+    "maize_sweet_long": {
+        "t_base": 10,
+        "t_upper": 32,
+        "stages": {
+            "initial": 355,
+            "development": 855,
+            "mid_season": 1285,
+            "harvest": 1485,
+        },
+    },
+    "maize_sweet_short": {
+        "t_base": 10,
+        "t_upper": 32,
+        "stages": {
+            "initial": 200,
+            "development": 510,
+            "mid_season": 870,
+            "harvest": 985,
+        },
+    },
+    "melon_long": {
+        "t_base": 10,
+        "t_upper": 38,
+        "stages": {
+            "initial": 140,
+            "development": 685,
+            "mid_season": 1145,
+            "harvest": 1600,
+        },
+    },
+    "melon_short": {
+        "t_base": 10,
+        "t_upper": 38,
+        "stages": {
+            "initial": 185,
+            "development": 705,
+            "mid_season": 1020,
+            "harvest": 1175,
+        },
+    },
+    "oats_common": {
+        "t_base": 0,
+        "t_upper": 30,
+        "stages": {
+            "initial": 450,
+            "development": 1045,
+            "mid_season": 1595,
+            "harvest": 1850,
+        },
+    },
+    "onion_common": {
+        "t_base": 4.5,
+        "t_upper": 35,
+        "stages": {
+            "initial": 460,
+            "development": 930,
+            "mid_season": 1810,
+            "harvest": 2290,
+        },
+    },
+    "pea_fresh_industry": {
+        "t_base": 5,
+        "t_upper": 27,
+        "stages": {
+            "initial": 90,
+            "development": 390,
+            "mid_season": 900,
+            "harvest": 925,
+        },
+    },
+    "pea_fresh_market": {
+        "t_base": 5,
+        "t_upper": 27,
+        "stages": {
+            "initial": 155,
+            "development": 505,
+            "mid_season": 1080,
+            "harvest": 1400,
+        },
+    },
+    "potato_long": {
+        "t_base": 2,
+        "t_upper": 30,
+        "stages": {
+            "initial": 405,
+            "development": 935,
+            "mid_season": 1425,
+            "harvest": 2260,
+        },
+    },
+    "potato_short": {
+        "t_base": 2,
+        "t_upper": 30,
+        "stages": {
+            "initial": 260,
+            "development": 750,
+            "mid_season": 1275,
+            "harvest": 1600,
+        },
+    },
+    "rice_long": {
+        "t_base": 12,
+        "t_upper": 35,
+        "stages": {
+            "initial": 255,
+            "development": 745,
+            "mid_season": 1485,
+            "harvest": 1805,
+        },
+    },
+    "rice_short": {
+        "t_base": 12,
+        "t_upper": 35,
+        "stages": {
+            "initial": 235,
+            "development": 700,
+            "mid_season": 1365,
+            "harvest": 1660,
+        },
+    },
+    "sorghum_long": {
+        "t_base": 10,
+        "t_upper": 30,
+        "stages": {
+            "initial": 280,
+            "development": 590,
+            "mid_season": 1240,
+            "harvest": 1520,
+        },
+    },
+    "sorghum_short": {
+        "t_base": 10,
+        "t_upper": 30,
+        "stages": {
+            "initial": 280,
+            "development": 580,
+            "mid_season": 1150,
+            "harvest": 1380,
+        },
+    },
+    "soybean_long": {
+        "t_base": 10,
+        "t_upper": 40,
+        "stages": {
+            "initial": 310,
+            "development": 720,
+            "mid_season": 1400,
+            "harvest": 1960,
+        },
+    },
+    "soybean_short": {
+        "t_base": 10,
+        "t_upper": 40,
+        "stages": {
+            "initial": 300,
+            "development": 650,
+            "mid_season": 1300,
+            "harvest": 1650,
+        },
+    },
+    "sunflower_long": {
+        "t_base": 8,
+        "t_upper": 30,
+        "stages": {
+            "initial": 385,
+            "development": 955,
+            "mid_season": 1640,
+            "harvest": 1905,
+        },
+    },
+    "sunflower_short": {
+        "t_base": 8,
+        "t_upper": 30,
+        "stages": {
+            "initial": 345,
+            "development": 900,
+            "mid_season": 1615,
+            "harvest": 1790,
+        },
+    },
+    "tomato_industry": {
+        "t_base": 7,
+        "t_upper": 28,
+        "stages": {
+            "initial": 280,
+            "development": 800,
+            "mid_season": 1680,
+            "harvest": 1900,
+        },
+    },
+    "tomato_market": {
+        "t_base": 7,
+        "t_upper": 28,
+        "stages": {
+            "initial": 325,
+            "development": 985,
+            "mid_season": 1865,
+            "harvest": 2065,
+        },
+    },
+    "wheat_spring_long": {
+        "t_base": 0,
+        "t_upper": 35,
+        "stages": {
+            "initial": 360,
+            "development": 1040,
+            "mid_season": 1760,
+            "harvest": 2300,
+        },
+    },
+    "wheat_spring_short": {
+        "t_base": 0,
+        "t_upper": 35,
+        "stages": {
+            "initial": 360,
+            "development": 840,
+            "mid_season": 1430,
+            "harvest": 1620,
+        },
+    },
+    "wheat_winter_long": {
+        "t_base": 0,
+        "t_upper": 35,
+        "stages": {
+            "initial": 560,
+            "development": 965,
+            "mid_season": 1695,
+            "harvest": 2105,
+        },
+    },
+    "wheat_winter_short": {
+        "t_base": 0,
+        "t_upper": 35,
+        "stages": {
+            "initial": 395,
+            "development": 840,
+            "mid_season": 1280,
+            "harvest": 1645,
         },
     },
 }
